60b091d71d0976fc2d9b18473932c3f76652862f
Daniel Arauz <sdarauz@gmail.com>
2018-12-14 00:55:53 +0100

    removing junk code in App.js

--- contacts-frontend/src/App.js
+++ contacts-frontend/src/App.js
@@ -3,37 +3,6 @@ import axios from 'axios';
 import ContactsList from './Contacts'
 import ContactForm from './ContactForm';
 
-const contactsArray = [
-  {
-    id: 0,
-    avatar: 'https://pbs.twimg.com/profile_images/1002272769352978433/9S4QWSR0_400x400.jpg',
-    username: 'SpongyBobu',
-    fullname: '',
-    phone: '+31 6 3400 7732'
-  },
-  {
-    id: 1,
-    avatar: 'https://i.ytimg.com/vi/krz7RZHnGnE/hqdefault.jpg',
-    username: 'elHijoDelPapa',
-    fullname: '',
-    phone: '+31 6 3400 7732'
-  },
-  {
-    id: 2,
-    avatar: 'https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg',
-    username: 'Puppy',
-    fullname: '',
-    phone: '+31 6 3400 7732'
-  },
-  {
-    id: 3,
-    avatar: 'https://yt3.ggpht.com/a-/AN66SAy6GVR9FKfi5C_qKDZkFSCyDIoxrhCeeWf-bQ=s900-mo-c-c0xffffffff-rj-k-no',
-    username: 'bob',
-    fullname: '',
-    phone: '+31 6 3400 7732'
-  },
-];
-
 class App extends Component {
   state = {
     contacts: [],
@@ -56,8 +25,6 @@ class App extends Component {
       }});
     });
   }
-
-
 
   render() {
     return (
